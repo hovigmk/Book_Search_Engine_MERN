28ab030cb0e5bfe81411446333354439034b1f93
Hovig Kalayjian <hovigkalayjian@gmail.com>
2023-03-24 20:07:16 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// Setting up atlas
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
